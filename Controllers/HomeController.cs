6ccf4f8368673e8d3bfeba462246953fc44a6a6b
Isaac Corcoran <46438401+IsaacCorky@users.noreply.github.com>
2021-02-01 21:43:05 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieCollection.Models;
+
+
+namespace MovieCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        public IActionResult AllMovies()
+        {
+            return View(Repository.Responses.Where(r => r.Title.ToLower() != "independance day"));
+        }
+
+        [HttpGet]
+        public IActionResult NewMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewMovie(NewMovie MovieObject) // Setting form data to an object
+        {
+            Repository.AddResponse(MovieObject); // Passing object to the "DB" (list) in a post method
+
+            if (ModelState.IsValid)
+            {
+                Response.Redirect("NewMovie"); // Reloads page
+            }
+            
+            return View("NewMovie", MovieObject); // Display view
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
